0db5ef5284b0d0b4abc09815737e15fa38547eee
airchaoz <airchaoz@outlook.com>
2025-04-12 10:26:37 +0800

    add time record impl

--- vector_ops/vadd.cu
+++ vector_ops/vadd.cu
@@ -17,12 +17,23 @@ int main() {
   for (int i = 0; i < n_size; i++) {
     int n = nums_n[i];
 
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
     float *d_a, *d_b, *d_c;
     CHECK(cudaMalloc((void **)&d_a, n * sizeof(float)));
     CHECK(cudaMalloc((void **)&d_b, n * sizeof(float)));
     CHECK(cudaMalloc((void **)&d_c, n * sizeof(float)));
 
+    cudaEventRecord(start);
     vadd<<<(n + 256) / 256, 256>>>(d_a, d_b, d_c, n);
+    cudaEventRecord(stop);
+
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    printf("The vector add(size %d) time elapsed: %f ms\n", n, milliseconds);
 
     cudaFree(d_a);
     cudaFree(d_b);
