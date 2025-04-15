643695b59945d8749d11aee8b750a769915342d4
airchaoz <airchaoz@outlook.com>
2025-04-15 22:00:25 +0800

    impl reduce_sum_v2

--- 02_reduce/reduce_sum.cu
+++ 02_reduce/reduce_sum.cu
@@ -1,5 +1,4 @@
 #include "bits/stdc++.h"
-#include "../helper/error.cuh"
 
 using namespace std;
 
@@ -42,13 +41,43 @@ __global__ void reduce_sum_v1(float *arr, float *result, int n) {
     }
 }
 
+__global__ void reduce_sum_v2(float *arr, float *result, int n) {
+
+    extern __shared__ float s_data[];
+    int tid = threadIdx.x;
+    int idx = blockDim.x * blockIdx.x + tid;
+
+    s_data[tid] = (idx < n) ? arr[idx] : 0.f;
+    __syncthreads();
+
+    for (int i = blockDim.x >> 1; i >= 32; i = i >> 1) {
+        if (tid < i) {
+            s_data[tid] += s_data[tid + i];
+        }
+        __syncthreads();
+    }
+
+    if (tid < 32) {
+        float val = s_data[tid];
+
+        for (int mask = 16; mask > 0; mask >>= 1) {
+            val += __shfl_down_sync(0xFFFFFFFF, val, mask);
+        }
+
+        if (tid == 0) {
+            atomicAdd(result, val);
+        }
+    }
+}
+
 void test_error(int n) {
 
     const int block_size = 128;
     const int grid_size = (n + block_size - 1) / block_size;
 
     float *h_a = (float *)malloc(n * sizeof(float));
-    float *h_result = (float *)malloc(sizeof(float));
+    float *v1_result = (float *)malloc(sizeof(float));
+    float *v2_result = (float *)malloc(sizeof(float));
 
     srand(42);
     for (int i = 0; i < n; i++) {
@@ -57,28 +86,35 @@ void test_error(int n) {
     float kahan_result = kahan_sum_single(h_a, n);
 
     float *d_a;
-    float *d_result;
+    float *d_v1_result;
+    float *d_v2_result;
 
     cudaMalloc((void **)&d_a, n * sizeof(float));
-    cudaMalloc((void **)&d_result, sizeof(float));
+    cudaMalloc((void **)&d_v1_result, sizeof(float));
+    cudaMalloc((void **)&d_v2_result, sizeof(float));
 
     cudaMemcpy(d_a, h_a, n * sizeof(float), cudaMemcpyHostToDevice);
 
-    reduce_sum_v1<<<grid_size, block_size, block_size * sizeof(float)>>>(d_a, d_result, n);
+    reduce_sum_v1<<<grid_size, block_size, block_size * sizeof(float)>>>(d_a, d_v1_result, n);
+    cudaMemcpy(v1_result, d_v1_result, sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(h_result, d_result, sizeof(float), cudaMemcpyDeviceToHost);
+    reduce_sum_v2<<<grid_size, block_size, block_size * sizeof(float)>>>(d_a, d_v2_result, n);
+    cudaMemcpy(v2_result, d_v2_result, sizeof(float), cudaMemcpyDeviceToHost);
 
     printf("the result of kahan is %f\n", kahan_result);
-    printf("the result of v1  is %f\n", *h_result);
+    printf("the result of v1    is %f\n", *v1_result);
+    printf("the result of v2    is %f\n", *v2_result);
 
     free(h_a);
-    free(h_result);
+    free(v1_result);
+    free(v2_result);
 
-    cudaFree(d_result);
+    cudaFree(d_v1_result);
+    cudaFree(d_v2_result);
     cudaFree(d_a);
 }
 
-void test_performance(int n) {
+float test_performance(int n, void (*gpu_reduce_sum) (float *arr, float *result, int n)) {
 
     const int block_size = 128;
     const int grid_size = (n + block_size - 1) / block_size;
@@ -107,7 +143,7 @@ void test_performance(int n) {
 
     int test_rounds = 5;
     for (int i = 0; i < test_rounds; i++) {
-        reduce_sum_v1<<<grid_size, block_size, block_size * sizeof(float)>>>(d_a, d_result, n);
+        gpu_reduce_sum<<<grid_size, block_size, block_size * sizeof(float)>>>(d_a, d_result, n);
     }
 
     cudaMemcpy(h_result, d_result, grid_size * sizeof(float), cudaMemcpyDeviceToHost);
@@ -116,17 +152,23 @@ void test_performance(int n) {
     cudaEventSynchronize(stop);
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
-    printf("The vector reduce_sum_v1(size %d) time elapsed: %f ms\n", n, milliseconds / float(test_rounds));
+    // printf("The vector reduce_sum_v1(size %d) time elapsed: %f ms\n", n, milliseconds / float(test_rounds));
 
     free(h_a);
     free(h_result);
 
     cudaFree(d_result);
     cudaFree(d_a);
+    return milliseconds / float(test_rounds);
 }
 
 int main() {
     const int n = 100'000'000;
+
     test_error(n);
-    test_performance(n);
+    float milliseconds;
+    milliseconds = test_performance(n, reduce_sum_v1);
+    printf("The vector reduce_sum_v1(size %d) time elapsed: %f ms\n", n, milliseconds);
+    milliseconds = test_performance(n, reduce_sum_v2);
+    printf("The vector reduce_sum_v2(size %d) time elapsed: %f ms\n", n, milliseconds);
 }
